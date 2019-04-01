e22325712b1f099668ec72e4b3f9c17fdaca36cc
ExplodingPotato <wojtek4p4@gmail.com>
2019-04-01 12:36:23 +0200

    Tank steering and prefab

---
+++ Tonk/Assets/Scripts/Steering.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+	[SerializeField] private float acceleration = 1;
+	[SerializeField] private float rotationSpeed = 5;
+	
+	private Rigidbody rb;
+	private WheelCollider[] wheels;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		wheels = GetComponentsInChildren<WheelCollider>();
+	}
+
+	private void FixedUpdate()
+	{
+		// Acceleration calculations
+		int groundedWheels = 0;
+		foreach (WheelCollider wheel in wheels)
+			if (wheel.isGrounded)
+				groundedWheels++;
+
+		float traction = groundedWheels / wheels.Length;
+
+		rb.AddForce(transform.forward * traction * acceleration * Input.GetAxis("Vertical"), ForceMode.Acceleration);
+
+		// Rotating the tank in an ugly way
+		//rb.angularVelocity = transform.up * Input.GetAxis("Horizontal") * rotationSpeed;
+
+		// Rotating the tank in a less ugly way
+		rb.AddTorque(transform.up * rotationSpeed * Input.GetAxis("Horizontal"), ForceMode.Acceleration);
+
+		// Slightly pulls the velocity towards forward direction
+		//Vector3 targetVelocity = transform.forward * Vector3.ProjectOnPlane(rb.velocity, Vector3.up).magnitude;
+		//targetVelocity.y = rb.velocity.y;
+		//rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, Time.fixedDeltaTime * 3);
+
+		// Stabilizes the tank (to be wheels-down)
+		//transform.rotation = Quaternion.Lerp(
+		//	transform.rotation,
+		//	Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, Vector3.up), Vector3.up), 
+		//	Time.fixedDeltaTime
+		//);
+	}
+}
