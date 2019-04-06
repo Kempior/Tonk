c16020e4ac7e38c035bd72bef4db6d2d93c90635
ExplodingPotato <wojtek4p4@gmail.com>
2019-04-06 14:24:39 +0200

    Added camera aiming scripts

---
+++ Tonk/Assets/Scripts/CameraAiming.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAiming : MonoBehaviour
+{
+	public GameObject AimingPoint;
+	public float AimingDistance = 100;
+
+	private new Camera camera;
+
+	private void Start()
+	{
+		camera = GetComponent<Camera>();
+
+		AimingPoint.transform.SetParent(null);
+	}
+
+	void Update()
+    {
+		// Cast a ray forward a set distance, set AimingPoint's position to the ray's hit point or to its end
+		Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+		int layerMask = ~LayerMask.GetMask("TankHull");
+		layerMask &= ~LayerMask.GetMask("TankTurret");
+
+		if (Physics.Raycast(ray, out RaycastHit hit, AimingDistance, layerMask))
+			AimingPoint.transform.position = hit.point;
+		else
+			AimingPoint.transform.position = ray.GetPoint(AimingDistance);
+    }
+}
---
+++ Tonk/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+	public float MouseSensitivity = 10;
+
+	private GameObject cameraGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cameraGO = GetComponentInChildren<Camera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.rotation = transform.rotation * (Quaternion.Euler(0, Input.GetAxis("Mouse X") * Time.fixedDeltaTime * MouseSensitivity, 0));
+
+		cameraGO.transform.rotation = cameraGO.transform.rotation * (Quaternion.Euler(-Input.GetAxis("Mouse Y") * Time.fixedDeltaTime * MouseSensitivity, 0, 0));
+	}
+}
