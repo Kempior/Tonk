a0db90b1cdda82aaf76d2ba4aa88f642bbae85e3
ExplodingPotato <wojtek4p4@gmail.com>
2019-04-15 12:16:09 +0200

    Projectile prefabs and shit

---
+++ Tonk/Assets/Scripts/Weapons/ProjectileTimed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTimed : Weapon
+{
+	public uint Time = 2;
+
+	private void Start()
+	{
+		Invoke(nameof(Fire), Time);
+	}
+
+	protected override void Fire()
+	{
+		Instantiate(SpawnedGO, transform.position, transform.rotation);
+
+		Destroy(this);
+	}
+
+	protected override void FireEffects()
+	{
+		throw new System.NotImplementedException();
+	}
+}
