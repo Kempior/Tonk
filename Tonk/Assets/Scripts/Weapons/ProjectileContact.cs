2a00407acc1e847c30ea3cca6a26a62d862dc244
ExplodingPotato <wojtek4p4@gmail.com>
2019-04-15 10:02:25 +0200

    Prefabs for turrets and specific projectiles

---
+++ Tonk/Assets/Scripts/Weapons/ProjectileContact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileContact : Weapon
+{
+	public int Damage = 1;
+
+	protected override void Fire()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	protected override void FireEffects()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Health otherHealth = collision.transform.GetComponent<Health>();
+		if (otherHealth != null)
+			otherHealth.Hit(Damage);
+
+		if(ProjectileSpawn == null)
+			ProjectileSpawn = gameObject;
+		if (SpawnedGO != null)
+			Instantiate(SpawnedGO, ProjectileSpawn.transform.position, ProjectileSpawn.transform.rotation);
+
+		Destroy(gameObject);
+	}
+}
